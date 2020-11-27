920452489279b13cc52741858cbefde284975e5a
Adam Wojciechowski <adam@webee.online>
2020-11-27 16:53:35 +0100

    Refactor -> used Blog to build content;

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -5,20 +5,17 @@ declare(strict_types=1);
 namespace WeBee\gCMS\Command;
 
 use DomainException;
-use IteratorAggregate;
-use League\CommonMark\CommonMarkConverter as MdParser;
-use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
-use Twig\Environment as TwigEnvironment;
-use Twig\Loader\FilesystemLoader as TwigLoader;
 use WeBee\gCMS\Config\GenerateCommandConfig;
-use WeBee\gCMS\Config\PageFileConfig;
-use WeBee\gCMS\Content\PageFile;
+use WeBee\gCMS\FlexContent\Types\Blog;
+use WeBee\gCMS\Helpers\FileSystem\DefaultFileSystem;
+use WeBee\gCMS\Parsers\DefaultContentParser;
+use WeBee\gCMS\Processors\DefaultConfigProcessor;
+use WeBee\gCMS\Templates\DefaultTemplateManager;
 
 class GenerateCommand extends Command
 {
@@ -31,28 +28,6 @@ class GenerateCommand extends Command
      * @var array<mixed> $config Command configuration
      */
     private $config = [];
-
-    private $twig;
-
-    /**
-     * @var array<mixed> $dependencies Dependencies used by command
-     */
-    private $dependencies;
-
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->dependencies = [
-            'configs' => [
-                'processor' => new Processor(),
-                'pageFileConfig' => new PageFileConfig(),
-                'commandConfig' => new GenerateCommandConfig(),
-            ],
-            'mdParser' => new MdParser(),
-            'fs' => new Filesystem(),
-        ];
-    }
 
     /**
      * @inheritDoc
@@ -72,23 +47,30 @@ class GenerateCommand extends Command
     {
         $this->loadConfig($input->getOption('config-file'));
 
-        $this->twig = new TwigEnvironment(
-            new TwigLoader($this->config['resources']['templates']),
-            ['debug' => true]
+        $basePath = $this->config['output']['relative'] ? '' : $this->config['output']['path'];
+        $staticPath = $basePath . $this->config['output']['static'];
+
+        $templateManager = new DefaultTemplateManager($this->config['resources']['templates'], ['debug' => true]);
+        $configProcessor = new DefaultConfigProcessor();
+        $contentParser = new DefaultContentParser();
+        $fs = new DefaultFileSystem();
+
+        $blog = new Blog($contentParser, $templateManager, $configProcessor, $fs);
+        $blog->load(
+            json_encode([
+                'name' => 'WeBee.School',
+                'slogan' => 'This is best slogan',
+                'path' => [
+                    'static' => $staticPath,
+                    'base' => $basePath,
+                ],
+                'config' => [
+                    'sourcePath' => $this->config['input']['path'],
+                ]
+            ]),
+            ['finder' => new Finder()]
         );
-        $this->twig->addExtension(new \Twig\Extension\DebugExtension());
-
-        $fs = $this->dependencies['fs'];
-        $outputPath = $this->config['output']['path'];
-        $staticPath = $outputPath . $this->config['output']['static'];
-
-        $fs->remove($outputPath);
-        $fs->mkdir([$outputPath]);
-        $fs->mirror($this->config['resources']['static'], $staticPath);
-
-        $this->parseFiles();
-
-        $output->writeln('Content generated!');
+        $blog->export($this->config['output']['path']);
 
         return Command::SUCCESS;
     }
@@ -104,134 +86,17 @@ class GenerateCommand extends Command
             throw new DomainException('Configuration file not found');
         }
 
-        $config = json_decode(
-            file_get_contents($configFilePath),
-            true
-        );
+        $config = json_decode(file_get_contents($configFilePath), true);
 
         if (null === $config) {
             throw new DomainException('Configuration file is not a valid JSON file');
         }
 
-        $this->config = $this
-            ->dependencies['configs']['processor']
-            ->processConfiguration(
-                $this->dependencies['configs']['commandConfig'],
+        $this->config = (new DefaultConfigProcessor())
+            ->process(
+                new GenerateCommandConfig(),
                 [$this->config, $config]
             )
         ;
-    }
-
-    /**
-     * Gets files for parsing.
-     *
-     * @return IteratorAggregate Files with content to parse
-     */
-    private function getFilesToParse(): IteratorAggregate
-    {
-        $finder = new Finder();
-
-        return $finder
-            ->ignoreVCS(true)
-            ->ignoreVCSIgnored(true)
-            ->ignoreUnreadableDirs(true)
-            ->in($this->config['input']['path'])
-            ->files()
-            ->name('*.page.md')
-            ->sortByModifiedTime()
-            ->reverseSorting()
-        ;
-    }
-
-    private function parseFiles()
-    {
-        $pages = [];
-        $categories = [];
-
-        $basePath = $this->config['output']['relative'] ? '' : $this->config['output']['path'];
-        $staticPath = $basePath . $this->config['output']['static'];
-
-        foreach ($this->getFilesToParse() as $file) {
-            $page = new PageFile(
-                $file,
-                $this->dependencies['mdParser'],
-                $this->dependencies['configs']['processor'],
-                $this->dependencies['configs']['pageFileConfig']
-            );
-
-            $pages[$page->slug()] = $page;
-
-            foreach ($page->categories() as $category) {
-                if (!array_key_exists($category, $categories)) {
-                    $categories[$category] = [
-                        'name' => $category,
-                        'slug' => sprintf('categories/%s', $category),
-                        'pages' => [],
-                    ];
-                }
-
-                $categories[$category]['pages'][] = $page;
-            }
-        }
-
-        foreach ($pages as $pageSlug => $page) {
-            $this->dependencies['fs']->dumpFile(
-                implode('', [$this->config['output']['path'], $page->targetPath(), $page->targetFileName(), '.html']),
-                $this->twig->render(
-                    'page.twig',
-                    [
-                        'link' => [
-                            'static' => $staticPath,
-                            'base' => $basePath,
-                        ],
-                        'categories' => array_values($categories),
-                        'page' => $page,
-                    ]
-                )
-            );
-        }
-
-        foreach (array_values($categories) as $category) {
-            dump($category['name'], count($category['pages']));
-            $this->dependencies['fs']->dumpFile(
-                implode('', [$this->config['output']['path'], '/', $category['slug'], '.html']),
-                $this->twig->render(
-                    'page_list.twig',
-                    [
-                        'link' => [
-                            'static' => $staticPath,
-                            'base' => $basePath,
-                        ],
-                        'categories' => array_values($categories),
-                        'page' => [
-                            'lang' => 'en',
-                            'title' => $category['name'],
-                            'tags' => [$category['name']],
-                            'pages' => $category['pages'],
-                        ],
-                    ]
-                )
-            );
-        }
-
-        $this->dependencies['fs']->dumpFile(
-            implode('', [$this->config['output']['path'], '/index.html']),
-            $this->twig->render(
-                'page_list.twig',
-                [
-                    'link' => [
-                        'static' => $staticPath,
-                        'base' => $basePath,
-                    ],
-                    'categories' => array_values($categories),
-                    'page' => [
-                        'lang' => 'en',
-                        'title' => 'Latest articles',
-                        'tags' => ['latest', 'articles', 'top10'],
-                        'pages' => $pages,
-                    ],
-                ]
-            )
-        );
     }
 }
