bdf99a72672eb21cff2701f10ab72132770c73bc
Adam Wojciechowski <adam@webee.online>
2020-11-27 16:59:28 +0100

    Refactor -> Added content interface and abstraction;

---
+++ src/FlexContent/AbstractContent.php
@@ -0,0 +1,284 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeBee\gCMS\FlexContent;
+
+use DomainException;
+use WeBee\gCMS\FlexContent\ContentInterface as ContentInterface;
+use WeBee\gCMS\FlexContent\ContentRelationInterface as ContentRelationInterface;
+use WeBee\gCMS\Helpers\FileSystem\FileSystemInterface;
+use WeBee\gCMS\Parsers\ContentParserInterface;
+use WeBee\gCMS\Processors\ConfigProcessorInterface;
+use WeBee\gCMS\Templates\TemplateManagerInterface;
+
+abstract class AbstractContent implements ContentInterface, ContentRelationInterface
+{
+    /**
+     * @var ParserInterface $contentParser
+     */
+    protected $contentParser;
+
+    /**
+     * @var TemplateManagerInterface $templateManager
+     */
+    protected $templateManager;
+
+    /**
+     * @var ConfigProcessorInterface $configProcessor
+     */
+    protected $configProcessor;
+
+    /**
+     * @var FileSystemInterface $configProcessor
+     */
+    protected $fs;
+
+    /**
+     * @var string $rawContent
+     */
+    protected $rawContent = '';
+
+    /**
+     * @var string $renderedContent
+     */
+    protected $renderedContent = '';
+
+    /**
+     * @var array<mixed> $additionalData
+     */
+    protected $additionalData = [];
+
+    /**
+     * @var array $contentParts [
+     *  @var array<ContentInterface> [RELATION_TYPE]
+     * ]
+     */
+    protected $contentParts = [
+        ContentRelationInterface::RELATION_CHILD => [],
+        ContentRelationInterface::RELATION_RELATED => [],
+        ContentRelationInterface::RELATION_PARENT => [null],
+    ];
+
+    /**
+     * @var string $slug
+     */
+    protected $slug = '';
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(
+        ContentParserInterface $contentParser,
+        TemplateManagerInterface $templatesManager,
+        ConfigProcessorInterface $configurationProcessor,
+        FileSystemInterface $fs
+    )
+    {
+        $this->contentParser = $contentParser;
+        $this->templateManager = $templatesManager;
+        $this->configProcessor = $configurationProcessor;
+        $this->fs = $fs;
+        $this->loadConfigDefinition();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function slug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function load(string $rawContent, array $additionalData = []): ContentInterface
+    {
+        $this->rawContent = $rawContent;
+        $this->additionalData = array_merge($this->additionalData, $additionalData);
+        $this->render();
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function loadPart(
+        string $rawContent,
+        array $additionalData = [],
+        string $relation = ContentRelationInterface::RELATION_CHILD,
+        ?string $typeName = null
+    ): ContentInterface
+    {
+        if (empty($typeName)) {
+            $typeName = self::class;
+        }
+
+        $content = $this->buildNewContentInstance($typeName);
+
+        $this->contentParts[$relation][] = $content;
+
+        if (ContentRelationInterface::RELATION_CHILD === $relation) {
+            $content->setParent($this);
+        }
+
+        return $content->load($rawContent, $additionalData);
+    }
+
+    /**
+     * Creates new instance of content - that will be part of current content.
+     *
+     * @param string $className Fully qualified class name
+     *
+     * @return ContentInterface New content instance
+     */
+    private function buildNewContentInstance(string $className): ContentInterface
+    {
+        if (!class_exists($className)) {
+            throw new DomainException(sprintf('Requested content type class "%s" does not exists', $className));
+        }
+
+        return new $className(
+            $this->contentParser,
+            $this->templateManager,
+            $this->configProcessor,
+            $this->fs
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function export(string $targetPath = 'output', array $exported = []): array
+    {
+        $slug = $this->slug();
+
+        if (in_array($slug, $exported)) {
+            dump('ALREADY EXPORTED for: ' . $slug);
+            return $exported;
+        }
+
+        $this->fs->dumpFile(sprintf('%s//%s.html', $targetPath, $slug), $this->renderedContent);
+
+        $exported[] = $slug;
+
+        foreach ($this->getAll() as $content) {
+            $exported = array_merge(
+                $exported,
+                $content->export($targetPath, $exported)
+            );
+        }
+
+        return $exported;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __toString(): string
+    {
+        return $this->renderedContent;
+    }
+
+    /**
+     * Loads configuration definition.
+     *
+     * Configuration definition will be a composite for particular content implementations.
+     * This is why below must be left empty and if in concrete class configuration is necessary
+     * then it must be overloaded. This way concrete classes will not need constructor modifications.
+     */
+    protected function loadConfigDefinition()
+    {
+    }
+
+    /**
+     * Renders content to its expected format.
+     */
+    protected function render()
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setParent(ContentInterface $parentContent): ContentRelationInterface
+    {
+        $this->contentParts[ContentRelationInterface::RELATION_PARENT][0] = $parentContent;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getParent(): ?ContentInterface
+    {
+
+        return $this->contentParts[ContentRelationInterface::RELATION_PARENT][0];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function appendChild(ContentInterface $childContent): ContentRelationInterface
+    {
+        $this->contentParts[ContentRelationInterface::RELATION_CHILD][] = $childContent;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getChildren(): array
+    {
+        return $this->contentParts[ContentRelationInterface::RELATION_CHILD];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function appendRelated(ContentInterface $relatedContent): ContentRelationInterface
+    {
+        $this->contentParts[ContentRelationInterface::RELATION_RELATED][] = $relatedContent;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRelated(): array
+    {
+        return $this->contentParts[ContentRelationInterface::RELATION_RELATED];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAll(): array
+    {
+        $contents = [];
+        $parent = $this->getParent();
+
+        if (!is_null($parent)) {
+            $contents[] = $parent;
+        }
+
+        return array_merge(
+            $contents,
+            $this->getChildren(),
+            $this->getRelated(),
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get(?string $whatToGet = null, $valueIfNotExists = null)
+    {
+        return $valueIfNotExists;
+    }
+}
---
+++ src/FlexContent/ContentRelationInterface.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeBee\gCMS\FlexContent;
+
+interface ContentRelationInterface
+{
+    /**
+     * Describe relation between two contents that are somehow connected but not as parent and child.
+     *
+     * @var string RELATION_RELATED
+     */
+    public const RELATION_RELATED = 'related';
+
+    /**
+     * Describe relation from parent content to its descendants - children.
+     *
+     * @var string RELATION_CHILD
+     */
+    public const RELATION_CHILD = 'child';
+
+    /**
+     * Describes relation from descendant (child) content to its parent.
+     *
+     * @var string RELATION_PARENT
+     */
+    public const RELATION_PARENT = 'parent';
+
+    /**
+     * Sets contents' parent.
+     *
+     * @param ContentInterface $parentContent
+     */
+    public function setParent(ContentInterface $parentContent): self;
+
+    /**
+     * Gets contents' parent.
+     *
+     * return null|ContentInterface Returns parent or null if content have no parent
+     */
+    public function getParent(): ?ContentInterface;
+
+    /**
+     * Appends child to contents' list of children.
+     *
+     * @param ContentInterface $childContent
+     */
+    public function appendChild(ContentInterface $childContent): self;
+
+    /**
+     * Gets all contents' children.
+     *
+     * @return array<ContentInterface>
+     */
+    public function getChildren(): array;
+
+    /**
+     * Appends related content.
+     *
+     * @param ContentInterface $relatedContent
+     */
+    public function appendRelated(ContentInterface $relatedContent): self;
+
+    /**
+     * Gets contents' related content.
+     *
+     * @return array<ContentInterface>
+     */
+    public function getRelated(): array;
+
+    /**
+     * Gets all contents' related content inc. parent and children.
+     *
+     * @return array<ContentInterface>
+     */
+    public function getAll(): array;
+
+}
