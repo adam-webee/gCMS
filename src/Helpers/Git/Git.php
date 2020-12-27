b66df923b8db272d146aae030498f70fd1a14a3c
Adam Wojciechowski <adam@webee.online>
2020-12-28 00:43:05 +0100

    Added Git repository handling helper;

---
+++ src/Helpers/Git/Git.php
@@ -0,0 +1,163 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeBee\gCMS\Helpers\Git;
+
+use DomainException;
+use WeBee\gCMS\Helpers\FileSystem\DefaultFileSystem;
+use WeBee\gCMS\Helpers\FileSystem\FileSystemInterface;
+use WeBee\gCMS\Helpers\Git\GitInterface;
+
+class Git implements GitInterface
+{
+    /**
+     * @var FileSystemInterface $fs
+     */
+    private $fs;
+
+    /**
+     * @var string $repositoryPath
+     */
+    private $repositoryPath;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(string $repositoryPath)
+    {
+        $this->repositoryPath = realpath($repositoryPath);
+
+        if (false === $this->repositoryPath) {
+            throw new DomainException(sprintf('Repository path "%s" does not exists', $repositoryPath));
+        }
+
+        $this->fs = new DefaultFileSystem();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isItGit(): bool
+    {
+        $result = $this->execute('status');
+
+        return preg_match('/.*not a git repository.*/', $result) ? false : true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function clone(string $uri)
+    {
+        if (false == file_exists($this->repositoryPath)) {
+            $this->fs->mkdir($this->repositoryPath);
+        }
+
+        if ($this->isItGit()) {
+            throw new DomainException(sprintf('%s already contains repository', $this->repositoryPath));
+        }
+
+        $result = $this->execute(sprintf('clone %s %s', $uri, $this->repositoryPath));
+        $cloned = preg_match(sprintf("/Cloning into/", $this->repositoryPath), $result) ? true : false;
+
+        if (
+            !$cloned
+            || !$this->isItGit()
+            || $this->isDirty()
+        ) {
+            throw new DomainException(sprintf('Can not clone repository "%s" into %s', $uri, $this->repositoryPath));
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkout(string $branch = 'master', bool $force = false)
+    {
+        if (!$this->isItGit()) {
+            throw new DomainException(sprintf('%s is not a git repository', $this->repositoryPath));
+        }
+
+        if ($this->isDirty()) {
+            $this->execute('checkout -- .');
+            $this->execute('reset HEAD .');
+            $this->execute('checkout -- .');
+
+            if ($this->isDirty()) {
+                throw new DomainException(sprintf('Can not change branch to "%s" in repository: %s', $branch, $this->repositoryPath));
+            }
+        }
+
+        $result = $this->execute('checkout ' . $branch);
+
+        if (
+            preg_match("/Already on '$branch'/", $result)
+            || preg_match("/Switched to branch '$branch'/", $result)
+        ) {
+            return;
+        }
+
+        throw new DomainException(sprintf('Cannot switch to branch "%s" in repository: %s', $branch, $this->repositoryPath));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function pull(bool $fastForward = true)
+    {
+        if ($this->isDirty()) {
+            throw new DomainException(sprintf('Can not pull on dirty repository: %s', $this->repositoryPath));
+        }
+
+        $pullCommand = 'pull';
+
+        if ($fastForward) {
+            $pullCommand .= ' -ff';
+        }
+
+        $result = $this->execute($pullCommand);
+
+        $alreadyUpdated = preg_match('/Already up to date/', $result) ? true : false;
+        $pullApplied = preg_match('/From[\s\S]*Updating/m', $result) ? true : false;
+
+        if (!$alreadyUpdated || !$pullApplied) {
+            throw new DomainException(sprintf('Can not pull changes into repository: %s', $this->repositoryPath));
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete()
+    {
+        $this->fs->remove($this->repositoryPath);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isDirty(): bool
+    {
+        $result = $this->execute('status');
+
+        return preg_match('/.*nothing to commit, working tree clean.*/', $result) ? false : true;
+    }
+
+    /**
+     * Will execute provided command.
+     *
+     * @param string $command Command to execute
+     *
+     * @return null|string Result of command execution (e.g. command output)
+     */
+    private function execute(string $command): ?string
+    {
+        $command = sprintf('git -C %s %s 2>&1', $this->repositoryPath, $command);
+        $output = [];
+        $exeCode = 0;
+        exec($command, $output, $exeCode);
+
+        return join("\n", $output);
+    }
+}
---
+++ src/Helpers/Git/GitInterface.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeBee\gCMS\Helpers\Git;
+
+interface GitInterface
+{
+    /**
+     * Initialize class instance.
+     *
+     * @param string $repositoryPath Relative or full path to the folder for repository
+     */
+    public function __construct(string $repositoryPath);
+
+    /**
+     * Verifies if provided repository path holds actual repository or not.
+     *
+     * @return bool True, if it is git repository
+     */
+    public function isItGit(): bool;
+
+    /**
+     * Clones repository from provided source.
+     *
+     * @param string $uri Remote repository address
+     */
+    public function clone(string $uri);
+
+    /**
+     * Change current branch to defined one.
+     *
+     * @param string $branchName [opt] Name of the branch to switch to. By default 'master'
+     * @param bool $force [opt] If true, reverts/removes blocking changes to allow checkout. Default 'false'
+     *
+     * @throws DomainException if cannot change branch
+     */
+    public function checkout(string $branch = 'master', bool $force = false);
+
+    /**
+     * Pulls current version from remote repository.
+     *
+     * @param bool $fastForward Will use fast forward strategy for merge.
+     */
+    public function pull(bool $fastForward = true);
+
+    /**
+     * Deletes local repository - by deleting repository folder and all its sub-folders.
+     */
+    public function delete();
+
+    /**
+     * @return bool True, if current repository state is not clean (e.g. local changes)
+     */
+    public function isDirty(): bool;
+}
